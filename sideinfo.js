e9f70ac1dc8a8b513e035705d010ea1fe9414871
VoidArchivesWiki <123560581+VoidArchivesWiki@users.noreply.github.com>
2023-04-04 20:45:53 +1000

    Add files via upload

--- sideinfo.js
+++ sideinfo.js
@@ -5,3 +5,11 @@ fetch("Void-Archives-Honkai-Wiki/sideinfo.html")
   .then(data => {
     document.querySelector("sideBarHI").innerHTML = data;
   });
+  
+fetch("Void-Archives-StarRail-Wiki/sideinfo.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("sideBarSR").innerHTML = data;
+  });
